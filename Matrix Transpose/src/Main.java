e8adfa528ad0a93fd9e6db5a98ec56947816a057
suleymankayir <suleymantalhakayir@gmail.com>
2023-12-20 12:41:12 +0300

    Repeated num commit

---
+++ Matrix Transpose/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3}, {4, 5, 6,}};
+        int[][] list = new int[arr[0].length][arr.length];
+
+    }
+}
