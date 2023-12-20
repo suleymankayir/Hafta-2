a83e40602f180d705321c4a5f81fc42a30892eb9
suleymankayir <suleymantalhakayir@gmail.com>
2023-12-20 13:47:06 +0300

    Comment commit

--- Matrix Transpose/src/Main.java
+++ Matrix Transpose/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6,},{7,8,9}};
+        System.out.println("Tranpozu alınacak dizi: "+Arrays.deepToString(arr));
         translateToMatrix(arr);
 
     }
@@ -11,11 +12,11 @@ public class Main {
 
 
         int [][] list = new int[arr[0].length][arr.length];
-        for (int i = 0; i < list.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+        for (int i = 0; i < list.length; i++) { // row
+            for (int j = 0; j < arr.length; j++) { // column
                 list[i][j] = arr[j][i];
             }
-            System.out.println(Arrays.toString(list[i]));
+            System.out.println(Arrays.toString(list[i])); // transposed array
         }
 
 
